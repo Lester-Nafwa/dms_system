48f4722d0555b3ceab00ac4a0187f86ee38a6f8c
Lester-Nafwa <nafwalester@gmail.com>
2023-08-14 21:01:42 +0300

    house keeping

---
+++ dms/server.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const app = express();
+const fs = require("fs"); // Import the fs module
+const path = require("path"); // Import the path module
+
+const corsOptions = {
+  origin: "http://10.50.238.145:8080", // Adjust this to match your Vue development server's origin
+  methods: "GET, POST, PUT, DELETE",
+  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+const fileFilter = (_req, file, cb) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "application/pdf",
+  ];
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error("Incorrect file");
+    error.code = "INCORRECT_FILETYPE";
+    return cb(error, false);
+  }
+  cb(null, true);
+};
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, path.join( "uploads"));
+  },
+  filename: function (_req, file, cb) {
+    cb(null, file.fieldname + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter,
+  limits: {
+    fileSize: 10000000,
+  },
+});
+
+const PORT = process.env.PORT || "3000";
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    console.log("No file received");
+    res.status(400).json({ error: "wrong format" });
+    return;
+  }
+  if (req.file) {
+    console.log("File uploaded successfully:", req.file.filename);
+    res.json({
+      message: "File uploaded successfully",
+      filename: req.file.filename,
+    });
+  } else {
+    console.log("File upload failed");
+    res.status(500).json({ error: "File upload failed" });
+  }
+});
+console.log("hit");
+app.use((err, _req, res, _next) => {
+  if (err.code === "INCORRECT_FILETYPE") {
+    res.status(422).json({ error: "Only images and PDFs are allowed" }); 
+    return;
+  }
+  if (err.code === "LIMIT_FILE_SIZE") {
+    res.status(422).json({ error: "Maximum file size is 10MB" });
+    return;
+  }
+});
+
+app.get("/api/upload/data", (_req, res) => {
+  const directoryPath = path.join(__dirname, "uploads");
+  
+  
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      res.status(500).json({ error: "Error reading directory" });
+      return;
+    }
+
+    const imageFiles = files.filter(file => {
+      const fileExtension = path.extname(file).toLowerCase();
+      return [".jpg", ".jpeg", ".png"].includes(fileExtension);
+    });
+
+    res.json({ message: "Image files retrieved successfully", files: imageFiles });
+  });
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- dms/src/components/download.vue
+++ dms/src/components/download.vue
@@ -1,93 +1,106 @@
 <template>
-  
   <form @submit.prevent="onSubmit" enctype="multipart/form-data">
-    <div class="imagePreviewWrapper" :style="{ 'background-image': `url(${previewImage})` }" @click="selectFile">
+    <div
+      class="imagePreviewWrapper"
+      :style="{ 'background-image': `url(${previewImage})` }"
+      @click="selectFile"
+    ></div>
+
+    <input ref="fileInput" type="file" @input="pickFile" @change="onSelect" />
+    <br />
+    <div class="fields">
+      <button>Submit</button>
     </div>
-    
-    <input ref="fileInput" type="file" @input="pickFile"   @change="onSelect"   >
-  <br>
-   <div class="fields">
-        <button>Submit</button>
-      </div><br>
-      <div class="message">
-        <h5>{{message}}</h5>
- 
-   </div>
-   </form>
+    <br />
+    <div class="message">
+      <h5>{{ message }}</h5>
+    </div>
+  </form>
 </template>
- 
+
 <script>
- import axios from 'axios';
-    export default {
-  name: 'FileUpload',
+import axios from "axios";
+export default {
+  name: "FileUpload",
   data() {
     return {
-      file:"",
-      message:"",
-      previewImage:null
-    }
+      file: "",
+      message: "",
+      previewImage: null,
+    };
   },
-  methods: { 
-   selectFile () {
-          this.$refs.fileInput.click()
-      },
-      pickFile () {
-        let input = this.$refs.fileInput
-        let file = input.files
-        if (file && file[0]) {
-          let reader = new FileReader
-          reader.onload = e => {
-            this.previewImage = e.target.result
-          }
-          reader.readAsDataURL(file[0])
-          this.$emit('input', file[0])
-        }
-      },
-
- onSelect(){
-      const allowedTypes = ["image/jpeg", "image/jpg", "image/png",".pdf,.doc"];
-      const file = this.$refs.fileInput.files[0];
-      this.file = file;
-      if(!allowedTypes.includes(file.type)){
-        this.message = "Filetype is wrong!!"
-      }
-      if(file.size>10000000){
-        this.message = 'Too large, max size allowed is 500kb'
+  methods: {
+    selectFile() {
+      this.$refs.fileInput.click();
+    },
+    pickFile() {
+      let input = this.$refs.fileInput;
+      let file = input.files;
+      if (file && file[0]) {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          this.previewImage = e.target.result;
+        };
+        reader.readAsDataURL(file[0]);
+        this.$emit("input", file[0]);
       }
     },
 
-async onSubmit(){
+    onSelect() {
+      const allowedTypes = [
+        "image/jpeg",
+        "image/jpg",
+        "image/png",
+        "application/pdf",
+        "application/msword",
+      ];
+
+      const file = this.$refs.fileInput.files[0];
+      this.file = file;
+      if (!allowedTypes.includes(file.type)) {
+        this.message = "Filetype is wrong!!";
+      }
+      if (file.size > 10000000) {
+        this.message = "Too large, max size allowed is 500kb";
+      }
+    },
+
+    async onSubmit() {
+      if (!this.file) {
+        this.message = "No file selected";
+        return;
+      }
       const formData = new FormData();
-      formData.append('file',this.file);
-      try{
-        await axios.post('http://localhost:5000/upload',formData);
-        this.message = 'Uploaded!!'
-      }
-      catch(err){
+      formData.append("file", this.file);
+
+      try {
+        await axios.post("http://localhost:3000/api/upload", formData);
+        this.message = "Uploaded!!";
+      } catch (err) {
         console.log(err);
-        this.message = err.response.data.error
+        this.message = err.response.data.error;
       }
-    }
+    },
   },
-}
+};
 </script>
- 
+
 <style scoped>
 .imagePreviewWrapper {
-    width: 40%;
-    height:18em;
-    cursor: pointer;
-    margin: 0 auto 30px;
-    background-size: cover;
-    background-position: center center;
+  width: 40%;
+  height: 18em;
+  cursor: pointer;
+  margin: 0 auto 30px;
+  background-size: cover;
+  background-position: center center;
 }
-form {margin: auto;
+form {
+  margin: auto;
   width: 100%;
   border: 3px solid grey;
   padding: 10px;
   width: 50%;
-   text-align: center;
-   background:#F5F5F5;
-  }
+  text-align: center;
+  background: #f5f5f5;
+}
 </style>
-  
